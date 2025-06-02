98fcc8f8c4c112b0d6257f937313ff287a25c0d7
fernando-rosa-wmw <fernando.rosa@wmw.com.br>
2025-06-02 14:24:40 -0300

    Implementar change de checkbox selecionada.

--- src/app/presentation/view/tab1/todo-list/todo-list.component.ts
+++ src/app/presentation/view/tab1/todo-list/todo-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed, Input, Signal, signal, WritableSignal } from '@angular/core';
 import { IonGrid, IonCol, IonCheckbox, IonRow } from "@ionic/angular/standalone";
+import { CheckboxChangeEventDetail } from '@ionic/angular';
 import { Todo } from 'src/app/structure/todo/domain/entity/todo';
 
 @Component({
@@ -17,7 +18,23 @@ export class TodoListComponent {
   @Input() todoList: WritableSignal<Array<Todo>> = signal([]);
 
   doneTodoList: Signal<Todo[]> = computed(() => this.todoList().filter(todo => todo.done));
-  penddingTodoList: Signal<Todo[]> = computed(() => this.todoList().filter(todo => ! todo.done));;
+  penddingTodoList: Signal<Todo[]> = computed(() => this.todoList().filter(todo => ! todo.done));
 
   constructor() { }
+
+  toggleTodo(done: boolean, todoId: string) {
+    this.todoList.update(todos =>
+      todos.map(todo =>{        
+        if (todo.id === todoId) {
+          return { ...todo, done: done } 
+        } else {
+          return todo;
+        }
+      })
+    );
+  }
+
+  handleCheckBox(event: CheckboxChangeEventDetail<boolean>, todoId: string) {
+    this.toggleTodo(event.checked, todoId);
+  }
 }
